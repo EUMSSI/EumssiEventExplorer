9541f26498c7663c5c7e07281652cc82095b0a2f
Mainul Quraishi <mainulquraishi@Mainuls-MacBook-Air.local>
2016-01-21 13:29:19 +0100

    new update

--- src/main/java/de/l3s/eumssi/action/textsyncAction.java
+++ src/main/java/de/l3s/eumssi/action/textsyncAction.java
@@ -132,7 +132,7 @@ public class textsyncAction implements Action, ServletRequestAware {
 		//template for questions of locations
 		locationMapQuestion.put("currency", "What is the name of the currency?");
 		locationMapQuestion.put("officialLanguage", "What is the official language spoken here?");
-	//	locationMapQuestion.put("languages", "Which language/languages are spoken?");
+		locationMapQuestion.put("languages", "Which language/languages are spoken?");
 		locationMapQuestion.put("neighbours", "Which countries are the neighbours?");
 		locationMapQuestion.put("timezone", "In which timezone is this city located?");
 		locationMapQuestion.put("capital", "What is the name of the capital?");
@@ -235,6 +235,9 @@ public class textsyncAction implements Action, ServletRequestAware {
 							makeData(question, i + 1, thumbnail);
 							continue;
 					}
+					else if(mainKeyForQuestion.equals("population") && type.equals("country"))
+						continue;
+					
 					correctAns = mainKeyValue;
 			        //for locations
 					if(type.equals("city") || type.equals("country") || type.equals("location")){
